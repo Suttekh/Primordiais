56cd8f1a6d374d68839ed3d21e4054bf21ad28ac
EndrilsBlackk <nasuscifao9@gmail.com>
2020-10-20 16:47:15 -0300

    pri 1.5

--- corpo.php
+++ corpo.php
@@ -4,12 +4,70 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="public/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="public/css/pri.css">
 
 
     <title>Alteração number 1</title>
 
 </head>
 <body>
+<!-- CABEÇALHOOOO -->
+<div class="container-fluid">
+<div class="row justify-content-center">
+
+<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGzOAUBckVm3pHtnrIjMxVzMQwZ4cwefidhg&usqp=CAU" class="py-1 rounded-circle" alt="">
+
+</div>
+</div>
+<!-- FIM DO CABEÇALHOOOO -->
+
+
+
+
+
+<div class="py-3 row justify-content-center">
+
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/6-60449_jurassic-fight-club.jpg" alt="Primeiro Slide">
+      <div class="carousel-caption d-none d-md-block">
+    <h5>ajsdnfkjnasdkjfnkjsadnkf</h5>
+    <p>aksdnfkjnasdkjfnasdfk</p>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/13-133698_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom.png" alt="Segundo Slide">
+      <div class="carousel-caption d-none d-md-block">
+   <h5>nfkjasdnfaksdjnfkjasd</h5>
+   <p>asldkfmnklasdmflkasmdf</p>
+    </div>
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/13-133699_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom-cake.png" alt="Terceiro Slide">
+      <div class="carousel-caption d-none d-md-block">
+      <h5>ajksdnfkjasdnfjkasndkjf</h5>
+      <p>anskjdnfkjasdnfadsf</p>
+        </div>
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Anterior</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Próximo</span>
+  </a>
+</div>
+</div>
+
+
     <br>
     <br>
     <!-- CARDS -->
