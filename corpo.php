3aab20e70cdbcd4f82ced00bc77952f0425d0781
Suttekh <josueferrrreira@hotmail.com>
2020-10-22 15:14:45 -0300

    Primordiais 3.0

--- corpo.php
+++ corpo.php
@@ -1,149 +1,176 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="public/css/pri.css">
-
-
-    <title>Alteração number 1</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="public/bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="public/css/pri.css">
+  <link rel="icon" href="https://img2.gratispng.com/20180928/hcu/kisspng-logo-film-jurassic-park-pencil-jurassic-park-saga-a-cinematic-universe-5bae09f8a00826.0478525715381324726555.jpg">
+  <link rel="stylesheet" href="public/fontes/haloween.ttf">
+  <title>Alteração number 1</title>
 
 </head>
+
 <body>
-<!-- CABEÇALHOOOO -->
-<div class="container-fluid">
-<div class="row justify-content-center">
 
-<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQGzOAUBckVm3pHtnrIjMxVzMQwZ4cwefidhg&usqp=CAU" class="py-1 rounded-circle" alt="">
+  <header>
+
+      <!-- CABEÇALHOOOO -->
+      <div id="header" class="container-fluid">
+  
+      <img class="jura rounded-circle" src="public/img/jura.jpg" width="300" height="200" alt="">
 
 </div>
-</div>
-<!-- FIM DO CABEÇALHOOOO -->
+
+      <!-- FIM DO CABEÇALHOOOO -->
+  </header>
 
 
 
 
+  <div class="py-3 row justify-content-center">
 
-<div class="py-3 row justify-content-center">
-
-<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-  <ol class="carousel-indicators">
-    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-  </ol>
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/6-60449_jurassic-fight-club.jpg" alt="Primeiro Slide">
-      <div class="carousel-caption d-none d-md-block">
-    <h5>ajsdnfkjnasdkjfnkjsadnkf</h5>
-    <p>aksdnfkjnasdkjfnasdfk</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block" src="https://wi.wallpapertip.com/wsimgs/13-133698_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom.png" height="300px"
-      width="1000px" alt="Segundo Slide">
-      <div class="carousel-caption d-none d-md-block">
-   <h5>nfkjasdnfaksdjnfkjasd</h5>
-   <p>asldkfmnklasdmflkasmdf</p>
-    </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/13-133699_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom-cake.png" alt="Terceiro Slide">
-      <div class="carousel-caption d-none d-md-block">
-      <h5>ajksdnfkjasdnfjkasndkjf</h5>
-      <p>anskjdnfkjasdnfadsf</p>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/6-60449_jurassic-fight-club.jpg" alt="Primeiro Slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>aaaaaaaaaaaaaaaaa</h5>
+            <p>aksdnfkjnasdkjfnasdfk</p>
+          </div>
         </div>
+        <div class="carousel-item">
+          <img class="d-block" src="https://wi.wallpapertip.com/wsimgs/13-133698_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom.png" height="300px" width="1000px" alt="Segundo Slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>nfkjasdnfaksdjnfkjasd</h5>
+            <p>asldkfmnklasdmflkasmdf</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img class="d-block w-100" src="https://wi.wallpapertip.com/wsimgs/13-133699_fallen-kingdom-wallpapers-jurassic-world-fallen-kingdom-cake.png" alt="Terceiro Slide">
+          <div class="carousel-caption d-none d-md-block">
+            <h5>ajksdnfkjasdnfjkasndkjf</h5>
+            <p>anskjdnfkjasdnfadsf</p>
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Anterior</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Próximo</span>
+      </a>
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Anterior</span>
-  </a>
-  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Próximo</span>
-  </a>
-</div>
-</div>
 
 
-    <br>
-    <br>
-    <!-- CARDS -->
-    <div class="row justify-content-center">
-<div class="col-2 card">
-  <img class="card-img-top" src="https://placehold.it/150x100" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+  <br>
+  <br>
+
+  <h1 id="esp"  class="text-center"><b>Espécimes de Bestas Primordiais </b></h1>
+  <br>
+  <!-- CARDS -->
+  <div class="row justify-content-center">
+    <div style="border: rgb(41, 109, 70) solid 2px;" id="card1" class="mr-4 col-2 card">
+      <img class="card-img-top pt-3" src="public/img/dunk.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 id="card1texto" class="card-title"><b> Dunkleosteus </b></h5>
+        <p id="card1texto" class="card-text">Um dos maiores peixes encouraçados que já existiu!</p>
+        <a id="conhecer" href="https://placehold.it/300x300" class="ml-4 my-4 btn btn-success"> Conhecer Mais</a>
+      </div>
+    </div>
+
+    <div style="border: rgb(41, 109, 70) solid 2px;" id="card2" class="mr-4 col-2 card">
+      <img class="card-img-top pt-3" src="public/img/quet.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 id="card1texto" class="card-title">Quetzalcoatlus</h5>
+        <p id="card1texto" class="card-text">Um dos maiores Pterossauros conhecidos.</p>
+        <a id="conhecer" href="#" class="ml-3 btn btn-success"> Conhecer Mais</a>
+      </div>
+    </div>
+
+    <div style="border: rgb(41, 109, 70) solid 2px;" id="card3" class="col-2 card">
+      <img class="card-img-top pt-3" src="public/img/carnota.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 id="card1texto" class="card-title">Carnotaurus</h5>
+        <p id="card1texto" class="card-text">O ''dragão'' que existiu!</p>
+        <a id="conhecer" href="#" class="btn btn-success ml-4 my-5">Conhecer Mais</a>
+      </div>
+    </div>
   </div>
-</div>
+  <!-- FIM DOS CARDS -->
+  <br>
+  <!-- CÍRCULOS -->
+  <h1 class="text-center pb-3">Achados Arqueológicos</h1>
+  <div class="row justify-content-center">
 
-<div class="col-2 card">
-  <img class="card-img-top" src="https://placehold.it/150x100" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+
+  <a href="https://www.qries.com/">
+         <img class="rounded-circle mr-5" alt="Qries" src="public/img/dunk.jpg"
+         width="200" height="200">
+         <h3>aaaaaaaaaaa</h3>
+      </a>
+      <a href="https://www.qries.com/">
+         <img class="rounded-circle mr-5" alt="Qries" src="public/img/dunk.jpg"
+         width="200" height="200">
+         <h3>aaaaaaaaaaa</h3>
+      </a>
+      <a href="https://www.qries.com/">
+         <img class="rounded-circle mr-5" alt="Qries" src="public/img/dunk.jpg"
+         width="200" height="200">
+         <h3>aaaaaaaaaaa</h3>
+      </a>
+      <a href="https://www.qries.com/">
+         <img class="rounded-circle" alt="Qries" src="public/img/dunk.jpg"
+         width="200" height="200">
+         <h3>aaaaaaaaaaa</h3>
+      </a>
+
+
+
   </div>
-</div>
 
-<div class="col-2 card">
-  <img class="card-img-top" src="https://placehold.it/150x100" alt="Card image cap">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-</div>
- <!-- FIM DOS CARDS -->
-<br>
-<!-- CÍRCULOS -->
-<h1 class="text-center pb-3">Evoluções</h1>
-<div class="row justify-content-center">
+ 
 
-<img src="https://placehold.it/100x100" class="pl-3 rounded-circle" alt="Desc da Img">
-<img src="https://placehold.it/100x100" class="pl-3 rounded-circle" alt="Desc da Img">
-<img src="https://placehold.it/100x100" class="pl-3 rounded-circle" alt="Desc da Img">
-<img src="https://placehold.it/100x100" class="pl-3 rounded-circle" alt="Desc da Img">
-<img src="https://placehold.it/100x100" class="pl-3 rounded-circle" alt="Desc da Img">
 
-<!-- FIM DOS CÍRCULOS -->
-</div>
+  <!-- FIM DOS CÍRCULOS -->
 
-<!-- RODAPÉ --> 
-<br>
-<br>
-<footer>
-<div style="background-image: url();" >
-<div class="container-fluid btn-dark" >
-<div class="row" >
-<div class="col" >
+  <!-- RODAPÉ -->
+  <br>
+  <br>
+  <footer>
+    <div style="background-image: url();">
+      <div class="container-fluid btn-dark">
+        <div class="row">
+          <div class="col">
 
-<ul>
-<h6>Desenvolvedores do Site</h6>
-<li>Andrew Matheus </li>  
-<li>Junior Ferreira</li>
-<li>Adryan Lucas</li>
+            <ul>
+              <h6>Desenvolvedores do Site</h6>
+              <li>Andrew Matheus </li>
+              <li>Junior Ferreira</li>
+              <li>Adryan Lucas</li>
 
-</ul>    
+            </ul>
 
 
 
-</div>
-</div>
-</div>
-</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    </div>
 
-
-
-</footer>
+  </footer>
 </body>
-<script src="public/js/jquery.js" ></script>
+<script src="public/js/jquery.js"></script>
 <script src="public/bootstrap/js/bootstrap.min.js"></script>
 
 </html>
